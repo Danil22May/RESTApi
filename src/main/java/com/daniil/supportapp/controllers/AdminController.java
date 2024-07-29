c740dc6e8f19d5cfa5bc99c95014586a60240e7f
Danil22May <daniilmej88@gmail.com>
2024-07-30 01:25:56 +0200

    database sql added

--- src/main/java/com/daniil/supportapp/controllers/AdminController.java
+++ src/main/java/com/daniil/supportapp/controllers/AdminController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @RestController
 @RequestMapping("/api/v1")
-@CrossOrigin(origins = "http://localhost:5173/")
+@CrossOrigin(origins = "http://localhost:5173")
 public class AdminController {
 
 	@Autowired
